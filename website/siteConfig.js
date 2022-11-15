179775148dde33741bcea65741e06be842c6c8c0
David Wang <95896449+killer6893@users.noreply.github.com>
2022-11-14 21:08:14 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,9 +13,9 @@ const users = [];
 
 const siteConfig = {
     title: 'David Wangs', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Hard working, Love tech.',
     profilePhoto: 'img/profile.png',
-    url: 'https://killer6893.github.io', // Your website URL
+    url: 'https://Killer6893.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'killer6893',
+    organizationName: 'Killer6893',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
